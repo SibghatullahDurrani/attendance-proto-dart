722fd0aaac1c96d64c151f4b7b59b53823cad7fa
Sibghatullah Durrani <sibghatullahdurrani007@gmail.com>
2025-12-03 17:48:36 +0500

    fixed

--- lib/attendance_proto_dart.dart
+++ lib/attendance_proto_dart.dart
@@ -5,43 +5,43 @@ library;
 
 export 'src/attendance_proto_dart_base.dart';
 
-export 'src/generated/auth/auth.pb.dart'
-export 'src/generated/auth/auth.pbenum.dart'
-export 'src/generated/auth/auth.pbgrpc.dart'
-export 'src/generated/auth/auth.pbjson.dart'
+export 'src/generated/auth/auth.pb.dart';
+export 'src/generated/auth/auth.pbenum.dart';
+export 'src/generated/auth/auth.pbgrpc.dart';
+export 'src/generated/auth/auth.pbjson.dart';
 
-export 'src/generated/gateway/gateway.pb.dart'
-export 'src/generated/gateway/gateway.pbenum.dart'
-export 'src/generated/gateway/gateway.pbgrpc.dart'
-export 'src/generated/gateway/gateway.pbjson.dart'
+export 'src/generated/gateway/gateway.pb.dart';
+export 'src/generated/gateway/gateway.pbenum.dart';
+export 'src/generated/gateway/gateway.pbgrpc.dart';
+export 'src/generated/gateway/gateway.pbjson.dart';
 
-export 'src/generated/school/school.pb.dart'
-export 'src/generated/school/school.pbenum.dart'
-export 'src/generated/school/school.pbgrpc.dart'
-export 'src/generated/school/school.pbjson.dart'
+export 'src/generated/school/school.pb.dart';
+export 'src/generated/school/school.pbenum.dart';
+export 'src/generated/school/school.pbgrpc.dart';
+export 'src/generated/school/school.pbjson.dart';
 
-export 'src/generated/user/user.pb.dart'
-export 'src/generated/user/user.pbenum.dart'
-export 'src/generated/user/user.pbgrpc.dart'
-export 'src/generated/user/user.pbjson.dart'
+export 'src/generated/user/user.pb.dart';
+export 'src/generated/user/user.pbenum.dart';
+export 'src/generated/user/user.pbgrpc.dart';
+export 'src/generated/user/user.pbjson.dart';
 
-export 'src/generated/messages/auth/messages.pb.dart'
-export 'src/generated/messages/auth/messages.pbenum.dart'
-export 'src/generated/messages/auth/messages.pbjson.dart'
+export 'src/generated/messages/auth/messages.pb.dart';
+export 'src/generated/messages/auth/messages.pbenum.dart';
+export 'src/generated/messages/auth/messages.pbjson.dart';
 
-export 'src/generated/messages/common/messages.pb.dart'
-export 'src/generated/messages/common/messages.pbenum.dart'
-export 'src/generated/messages/common/messages.pbjson.dart'
+export 'src/generated/messages/common/messages.pb.dart';
+export 'src/generated/messages/common/messages.pbenum.dart';
+export 'src/generated/messages/common/messages.pbjson.dart';
 
-export 'src/generated/messages/gateway/messages.pb.dart'
-export 'src/generated/messages/gateway/messages.pbenum.dart'
-export 'src/generated/messages/gateway/messages.pbjson.dart'
+export 'src/generated/messages/gateway/messages.pb.dart';
+export 'src/generated/messages/gateway/messages.pbenum.dart';
+export 'src/generated/messages/gateway/messages.pbjson.dart';
 
-export 'src/generated/messages/school/messages.pb.dart'
-export 'src/generated/messages/school/messages.pbenum.dart'
-export 'src/generated/messages/school/messages.pbjson.dart'
+export 'src/generated/messages/school/messages.pb.dart';
+export 'src/generated/messages/school/messages.pbenum.dart';
+export 'src/generated/messages/school/messages.pbjson.dart';
 
-export 'src/generated/messages/user/messages.pb.dart'
-export 'src/generated/messages/user/messages.pbenum.dart'
-export 'src/generated/messages/user/messages.pbjson.dart'
+export 'src/generated/messages/user/messages.pb.dart';
+export 'src/generated/messages/user/messages.pbenum.dart';
+export 'src/generated/messages/user/messages.pbjson.dart';
 // TODO: Export any libraries intended for clients of this package.
